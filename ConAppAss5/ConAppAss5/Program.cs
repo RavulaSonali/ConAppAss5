1a8000f5a46afca74004a4486d51d679cf1a122a
Ravula Sonali <ravula.sonali@gmail.com>
2024-01-11 16:26:37 +0530

    ConAppAss5

---
+++ ConAppAss5/ConAppAss5/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Movie
+{
+    public int ID { get; set; }
+    public string? Title { get; set; }
+
+
+}
+
+public class MovieRepository
+{
+    public List<Movie> movies;
+
+    public MovieRepository()
+    {
+        movies = new List<Movie>
+        {
+            new Movie { Title = "Harry Potter", ID = 1  },
+            new Movie { Title = "3 Idiots",ID=2 },
+            new Movie { Title = "Hi Nanna", ID=3 },
+            new Movie{Title="Don",ID=4}
+        };
+    }
+
+    public List<Movie> GetAllMovies()
+    {
+        return movies;
+    }
+
+    public Movie GetMovieById(int ID)
+    {
+        return movies.FirstOrDefault(m => m.ID == ID);
+    }
+}
+
+public class Program
+{
+    public static MovieRepository movieRepository;
+
+    public static void Main(string[] args)
+    {
+        movieRepository = new MovieRepository();
+
+        DisplayMenu();
+    }
+
+    public static void DisplayMenu()
+    {
+        Console.WriteLine("Please select an option:");
+        Console.WriteLine("1. List all movies");
+        Console.WriteLine("2. Get a movie by ID");
+        Console.WriteLine("3. Exit");
+
+        switch (Console.ReadLine())
+        {
+            case "1":
+                ListAllMovies();
+                break;
+            case "2":
+                GetMovieById();
+                break;
+            case "3":
+                Environment.Exit(0);
+                break;
+            default:
+                Console.WriteLine("Invalid option. Please try again.");
+                break;
+        }
+    }
+
+    public static void ListAllMovies()
+    {
+        Console.WriteLine("Movies:");
+        foreach (var movie in movieRepository.GetAllMovies())
+        {
+            Console.WriteLine($"{movie.Title} - {movie.ID}");
+        }
+
+        DisplayMenu();
+    }
+
+    public static void GetMovieById()
+    {
+        Console.WriteLine("Enter the movie title:");
+        int id = int.Parse(Console.ReadLine());
+
+        var movie = movieRepository.GetMovieById(id);
+        if (movie != null)
+        {
+            Console.WriteLine($"Movie: {movie.ID} - {movie.Title}");
+        }
+        else
+        {
+            Console.WriteLine("Movie not found.");
+        }
+
+        DisplayMenu();
+    }
+}
